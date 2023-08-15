0e0fdfac914dae645a05f30406cf9a9a266a6cf8
Adedunmola Oyewale <oyewaleadedunmola@gmail.com>
2023-08-15 20:28:19 +0100

    Add TryFrom implementation for Png
    Add TryFrom implementation for Png to transform bytes into a vector
    of Chunks and a constant header of 8 bytes.

--- src/png.rs
+++ src/png.rs
@@ -9,56 +9,66 @@ impl Png {
     pub const STANDARD_HEADER: [u8; 8] = [137, 80, 78, 71, 13, 10, 26, 10];
 }
 
-// impl TryFrom<&[u8]> for Png {
-//     type Error = Error;
-//     fn try_from(value: &[u8]) -> Result<Self> {
+impl TryFrom<&[u8]> for Png {
+    type Error = Error;
+    fn try_from(value: &[u8]) -> Result<Self> {
         
-//         let header: [u8; 8] = value[..8].try_into().unwrap();
+        let header: [u8; 8] = value[..8].try_into().unwrap();
 
-//         if header != Png::STANDARD_HEADER {
+        if header != Png::STANDARD_HEADER {
 
-//             return Err("Invalid header".into())
-//         }
+            return Err("Invalid header".into())
+        }
 
-//         let chunks: Vec<u8> = value[8..].try_into().unwrap();
+        let mut chunks: Vec<Chunk> = vec![];
+        let mut i = 8;
+        let crc_bytes_length: usize = 4;
+        
+        while i < value.len() && i != value.len() {
 
-//         Ok( Png { header, chunks: vec![1, 2, 3] } )
-//     }
-// }
+            let chunk = Chunk::try_from(&value[i..]).unwrap();
+            i = chunk.length() as usize + crc_bytes_length;
+
+            chunks.push(chunk);
+        }
+
+        Ok( Png { header, chunks } )
+    }
+}
 
 // #![allow(unused_variables)]
 // fn main() {
-// #[cfg(test)]
-// mod tests {
-//     use super::*;
-//     use crate::chunk_type::ChunkType;
-//     use crate::chunk::Chunk;
-//     use std::str::FromStr;
-//     use std::convert::TryFrom;
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::chunk_type::ChunkType;
+    use crate::chunk::Chunk;
+    use std::str::FromStr;
+    use std::convert::TryFrom;
 
-//     fn testing_chunks() -> Vec<Chunk> {
-//         let mut chunks = Vec::new();
+    // fn testing_chunks() -> Vec<Chunk> {
+    //     let mut chunks = Vec::new();
 
-//         chunks.push(chunk_from_strings("FrSt", "I am the first chunk").unwrap());
-//         chunks.push(chunk_from_strings("miDl", "I am another chunk").unwrap());
-//         chunks.push(chunk_from_strings("LASt", "I am the last chunk").unwrap());
+    //     chunks.push(chunk_from_strings("FrSt", "I am the first chunk").unwrap());
+    //     chunks.push(chunk_from_strings("miDl", "I am another chunk").unwrap());
+    //     chunks.push(chunk_from_strings("LASt", "I am the last chunk").unwrap());
 
-//         chunks
-//     }
+    //     chunks
+    // }
 
-//     fn testing_png() -> Png {
-//         let chunks = testing_chunks();
-//         Png::from_chunks(chunks)
-//     }
+    // fn testing_png() -> Png {
+    //     let chunks = testing_chunks();
+    //     Png::from_chunks(chunks)
+    // }
 
-//     fn chunk_from_strings(chunk_type: &str, data: &str) -> Result<Chunk> {
-//         use std::str::FromStr;
+    // fn chunk_from_strings(chunk_type: &str, data: &str) -> Result<Chunk> {
+    //     use std::str::FromStr;
 
-//         let chunk_type = ChunkType::from_str(chunk_type)?;
-//         let data: Vec<u8> = data.bytes().collect();
+    //     let chunk_type = ChunkType::from_str(chunk_type)?;
+    //     let data: Vec<u8> = data.bytes().collect();
 
-//         Ok(Chunk::new(chunk_type, data))
-//     }
+    //     Ok(Chunk::new(chunk_type, data))
+    // }
 
 //     #[test]
 //     fn test_from_chunks() {
@@ -68,23 +78,23 @@ impl Png {
 //         assert_eq!(png.chunks().len(), 3);
 //     }
 
-//     #[test]
-//     fn test_valid_from_bytes() {
-//         let chunk_bytes: Vec<u8> = testing_chunks()
-//             .into_iter()
-//             .flat_map(|chunk| chunk.as_bytes())
-//             .collect();
+    // #[test]
+    // fn test_valid_from_bytes() {
+    //     let chunk_bytes: Vec<u8> = testing_chunks()
+    //         .into_iter()
+    //         .flat_map(|chunk| chunk.as_bytes())
+    //         .collect();
 
-//         let bytes: Vec<u8> = Png::STANDARD_HEADER
-//             .iter()
-//             .chain(chunk_bytes.iter())
-//             .copied()
-//             .collect();
+    //     let bytes: Vec<u8> = Png::STANDARD_HEADER
+    //         .iter()
+    //         .chain(chunk_bytes.iter())
+    //         .copied()
+    //         .collect();
 
-//         let png = Png::try_from(bytes.as_ref());
+    //     let png = Png::try_from(bytes.as_ref());
 
-//         assert!(png.is_ok());
-//     }
+    //     assert!(png.is_ok());
+    // }
 
 //     #[test]
 //     fn test_invalid_header() {
@@ -439,5 +449,5 @@ impl Png {
 //         202, 28, 31, 66, 176, 235, 16, 0, 0, 0, 3, 82, 117, 83, 116, 104, 101, 121, 158, 176, 245,
 //         160, 0, 0, 0, 0, 73, 69, 78, 68, 174, 66, 96, 130,
 //     ];
-// }
+}
 // }
